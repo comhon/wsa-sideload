7e420f8b125aaad726b5e308730d02bf9fb1a766
Jan Štěpán <comhon.stepan@gmail.com>
2021-10-21 01:56:07 +0200

    WSA APK sideload scripts

---
+++ wsaconnect.ps1
@@ -0,0 +1,7 @@
+﻿Push-Location $PsScriptRoot
+
+.\platform-tools\adb connect 127.0.0.1:58526
+
+Pop-Location
+
+pause
---
+++ wsasideload.ps1
@@ -0,0 +1,13 @@
+﻿Push-Location $PsScriptRoot
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    Filter = 'Android App (*.apk)|*.apk'
+}
+$null = $FileBrowser.ShowDialog()
+
+.\platform-tools\adb install "$($FileBrowser.FileName)"
+
+Pop-Location
