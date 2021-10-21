6ee194fa5242ccb9758b42b5b42b28556df93fb3
Comhon <comhon.software@gmail.com>
2021-10-21 02:05:55 +0200

    Added support sideloading splitted APKs

--- wsasideload.ps1
+++ wsasideload.ps1
@@ -1,6 +1,4 @@
-﻿Push-Location $PsScriptRoot
-
-Add-Type -AssemblyName System.Windows.Forms
+﻿Add-Type -AssemblyName System.Windows.Forms
 
 $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
     InitialDirectory = [Environment]::GetFolderPath('Desktop') 
@@ -8,6 +6,18 @@ $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
 }
 $null = $FileBrowser.ShowDialog()
 
-.\platform-tools\adb install "$($FileBrowser.FileName)"
+push-location "$PSScriptRoot\platform-tools"
+
+$prefix = [io.path]::GetFileNameWithoutExtension("$($FileBrowser.FileName)")
+$dir = [io.path]::GetDirectoryName("$($FileBrowser.FileName)")
+
+$prefix
+
+$packages = foreach ( $item in (Get-item "$dir\$($prefix)*" | Sort-Object -Property Name).Name ) {'"'+"$dir\$item"+'"'}
+$packagelist = $packages -join " "
+
+write-host $packagelist
+
+Start-Process .\adb -argumentlist 'install-multiple', $packagelist -NoNewWindow -Wait
 
 Pop-Location
